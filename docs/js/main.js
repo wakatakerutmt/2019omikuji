555da864a49008a630a9534e72764ab88f5f47f4
Takuya Iida <wakatakerutmt@ezweb.ne.jp>
2019-01-02 00:17:37 +0900

    first commit

---
+++ docs/js/main.js
@@ -0,0 +1,24 @@
+var btn = document.getElementById('btn');
+var left = document.getElementById('left');
+var right = document.getElementById('right');
+
+btn.addEventListener('click', function() {
+  var results = ['矢沢永吉', '大吉', '小吉', '凶', '中吉'];
+  // var n = Math.floor(Math.random() * 4);
+  var n = Math.floor(Math.random() * results.length);
+  this.textContent = results[n];
+  if(n === 0){
+    left.innerHTML = '<img src="img/1.jpg">';
+    right.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/oIGeygsGX2I?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+  }else{
+    left.innerHTML = '';
+    right.innerHTML = '';
+  }
+});
+
+btn.addEventListener('mousedown', function() { //マウスが押されているときはmousedown
+  this.className = 'pushed';
+});
+btn.addEventListener('mouseup', function() {//マウスが離れているときはmouseup
+  this.className = '';
+});
