4059a36221c6e196ffc307aaa1a13a79b0e20d7a
Takuya Iida <wakatakerutmt@ezweb.ne.jp>
2019-01-02 01:39:34 +0900

    修正

--- docs/js/main.js
+++ docs/js/main.js
@@ -8,9 +8,11 @@ btn.addEventListener('click', function() {
   var n = Math.floor(Math.random() * results.length);
   this.textContent = results[n];
   if(n === 0){
+    this.style.color = "black";
     left.innerHTML = '<img src="img/1.jpg">';
     right.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/oIGeygsGX2I?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
   }else{
+    this.style.color = "white";
     left.innerHTML = '';
     right.innerHTML = '';
   }
